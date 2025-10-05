b4cb30394af54a3a89dc15309342b9c9f09bdc16
Step4runsneakers <cornerstore553@gmail.com>
2025-10-05 21:49:48 +0300

    Resolve merge conflict in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -16,10 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-a83b4193f099d67aa3c7dbeea080f54f2de5acbb
